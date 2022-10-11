a5004c3eb61f993845269b8e3f4fb96255aa9424
dawid-polak <dawidow.polak@gmail.com>
2022-10-11 08:01:25 +0200

    functions to add task to array tasks

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,18 @@
-console.log('hello typescript!');
+const mainInput = document.querySelector('#main-input');
+const mainBtn = document.querySelector('#main-btn');
+const tasks = [
+    {
+        name: 'Przebiec 10km',
+        category: 'sport',
+        done: false
+    }
+];
+const addTekstToTasks = (task) => {
+    tasks.push(task);
+};
+mainBtn.addEventListener('click', () => {
+    addTekstToTasks({
+        name: mainInput.value,
+        done: false
+    });
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,30 @@
-console.log('hello typescript!');
+const mainInput: HTMLInputElement = document.querySelector('#main-input');
+const mainBtn: HTMLButtonElement = document.querySelector('#main-btn');
+
+type Categories = "sport" | "work";
+
+interface Task {
+    name: string;
+    category?: Categories;
+    done: boolean;
+}
+
+const tasks: Task[] = [
+    {
+        name: 'Przebiec 10km',
+        category: 'sport',
+        done: false
+    }
+];
+
+
+const addTekstToTasks = (task: Task) => {
+    tasks.push(task);
+};
+
+mainBtn.addEventListener('click', () => {
+    addTekstToTasks({
+        name: mainInput.value,
+        done: false
+    })
+})
