7c02231843fe2cce591704123afb7a6876c74039
dawid-polak <dawidow.polak@gmail.com>
2022-10-19 21:09:10 +0200

    add edit/add categories

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,17 @@
 const mainInput = document.querySelector('#main-input');
 const mainBtn = document.querySelector('#main-btn');
 const listTask = document.querySelector('.tasks');
+const btnEdit = document.querySelector('#edit-category');
+const modelEdit = document.querySelector('.modal--select-category');
+const btnCloseEdit = document.querySelector('#close');
+const inputAddCategory = document.querySelector('#add--category-input');
+const inputColorCategory = document.querySelector('#add--category-input-color');
+const btnAddCategory = document.querySelector('#add--category-btn');
+const categoriesList = document.querySelector('.categories-modal');
+const sectionCategories = document.querySelector('.section-categories');
 ;
 const tasks = [];
+const categories = [];
 const addTekstToTasks = (task) => {
     tasks.push(task);
     deleteTasksInHtml();
@@ -19,6 +28,7 @@ mainBtn.addEventListener('click', () => {
     addTekstToTasks({
         name: mainInput.value,
         category: radioChecked ? addCategoryToTask(radioChecked.value) : "",
+        color: radioChecked.title,
         done: false
     });
     mainInput.value = '';
@@ -45,13 +55,7 @@ const deleteTasksInHtml = () => {
     listTask.innerHTML = '';
 };
 const addColor = (task, spanText) => {
-    if (task.category === 'sport') {
-        spanText.classList.add('category-blue');
-    }
-    if (task.category === 'work') {
-        spanText.classList.add('category-green');
-    }
-    ;
+    spanText.style.color = task.color;
 };
 // remove task after click delete in task
 listTask.addEventListener('click', (e) => {
@@ -72,3 +76,74 @@ listTask.addEventListener('click', (e) => {
     }
     ;
 });
+btnEdit.addEventListener('click', () => {
+    modelEdit.style.display = 'flex';
+});
+btnCloseEdit.addEventListener('click', () => {
+    modelEdit.style.display = 'none';
+});
+const addCategoryToCategories = (category) => {
+    categories.push(category);
+    renderListCategoryInEdit();
+    inputAddCategory.value = '';
+};
+const deleteCategoryInHtml = () => {
+    categoriesList.innerHTML = '';
+};
+btnAddCategory.addEventListener('click', () => {
+    deleteCategoryInHtml();
+    addCategoryToCategories({
+        name: inputAddCategory.value,
+        color: inputColorCategory.value
+    });
+    renderCategoriesToApp();
+});
+const renderListCategoryInEdit = () => {
+    categories.forEach(category => {
+        const text = document.createElement('p');
+        const btn = document.createElement('button');
+        text.innerText = category.name;
+        text.style.color = category.color;
+        text.classList.add('category');
+        btn.innerText = 'delete';
+        categoriesList.appendChild(text);
+        text.appendChild(btn);
+    });
+};
+const inputCategory = document.createElement('input');
+const labelCategory = document.createElement('label');
+categoriesList.addEventListener('click', (e) => {
+    const htmlElement = e.target;
+    if (htmlElement.tagName === 'BUTTON') {
+        const category = htmlElement.closest('.category');
+        const categoryText = category.innerHTML;
+        categories.forEach((category, index) => {
+            if (category.name === categoryText) {
+                categories.splice(index, 1);
+            }
+            const inputCategory = document.querySelector(`#radio-${category.name}`);
+            const labelCategory = document.querySelector(`#label-${category.name}`);
+            inputCategory.remove();
+            labelCategory.remove();
+        });
+        category.remove();
+    }
+    ;
+});
+const renderCategoriesToApp = () => {
+    categories.forEach(category => {
+        const inputCategory = document.createElement('input');
+        const labelCategory = document.createElement('label');
+        inputCategory.setAttribute('type', 'radio');
+        inputCategory.setAttribute('id', `radio-${category.name}`);
+        inputCategory.setAttribute('name', 'category');
+        inputCategory.setAttribute('title', category.color);
+        inputCategory.value = category.name;
+        labelCategory.setAttribute('for', `radio-${category.name}`);
+        labelCategory.setAttribute('id', `label-${category.name}`);
+        labelCategory.style.color = category.color;
+        labelCategory.innerHTML = category.name.toUpperCase();
+        sectionCategories.appendChild(inputCategory);
+        sectionCategories.appendChild(labelCategory);
+    });
+};
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,32 @@
 const mainInput: HTMLInputElement = document.querySelector('#main-input');
 const mainBtn: HTMLButtonElement = document.querySelector('#main-btn');
 const listTask: HTMLUListElement = document.querySelector('.tasks');
-
+const btnEdit: HTMLButtonElement = document.querySelector('#edit-category');
+const modelEdit: HTMLDivElement = document.querySelector('.modal--select-category');
+const btnCloseEdit: HTMLButtonElement = document.querySelector('#close');
+const inputAddCategory: HTMLInputElement = document.querySelector('#add--category-input');
+const inputColorCategory: HTMLInputElement = document.querySelector('#add--category-input-color')
+const btnAddCategory: HTMLButtonElement = document.querySelector('#add--category-btn');
+const categoriesList: HTMLDivElement = document.querySelector('.categories-modal');
+const sectionCategories: HTMLDivElement = document.querySelector('.section-categories');
 
 type Categories = "sport" | "work" | "";
 
+
 interface Task {
     name: string;
-    category?: Categories;
+    category: string;
+    color: string;
     done: boolean;
 };
 
+interface Category {
+    name: string;
+    color: string;
+}
+
 const tasks: Task[] = [];
+const categories: Category[] = [];
 
 const addTekstToTasks = (task: Task) => {
     tasks.push(task);
@@ -31,7 +46,8 @@ mainBtn.addEventListener('click', () => {
 
     addTekstToTasks({
         name: mainInput.value,
-        category: radioChecked ? addCategoryToTask(radioChecked.value) as Categories : "",
+        category: radioChecked ? addCategoryToTask(radioChecked.value) : "",
+        color: radioChecked.title,
         done: false
     });
 
@@ -52,6 +68,7 @@ mainBtn.addEventListener('click', () => {
         li.classList.add('task');
         spanText.classList.add('text');
         spanButton.classList.add('btn');
+        
 
         addColor(task, spanText);
 
@@ -68,11 +85,7 @@ mainBtn.addEventListener('click', () => {
  };
 
  const addColor = (task, spanText) => {
-    if (task.category === 'sport') {
-        spanText.classList.add('category-blue');
-    } if (task.category === 'work') {
-        spanText.classList.add('category-green');
-    };
+    spanText.style.color = task.color;
  };
 
 // remove task after click delete in task
@@ -94,3 +107,106 @@ mainBtn.addEventListener('click', () => {
         return;
     };
  });
+
+
+btnEdit.addEventListener('click', () => {
+    modelEdit.style.display = 'flex';
+}); 
+
+btnCloseEdit.addEventListener('click', () => {
+    modelEdit.style.display = 'none';
+});
+
+
+const addCategoryToCategories = (category: Category) => {
+    categories.push(category);
+    renderListCategoryInEdit();
+    inputAddCategory.value = '';
+};
+
+
+const deleteCategoryInHtml = () => {
+    categoriesList.innerHTML = ''
+};
+
+btnAddCategory.addEventListener('click', () => {
+
+    deleteCategoryInHtml();
+    addCategoryToCategories({
+        name: inputAddCategory.value,
+        color: inputColorCategory.value
+    });
+    renderCategoriesToApp();
+});
+
+const renderListCategoryInEdit = () => {
+
+    categories.forEach(category => {
+
+        const text: HTMLParagraphElement = document.createElement('p');
+        const btn: HTMLButtonElement = document.createElement('button');
+
+        text.innerText = category.name;
+        text.style.color = category.color;
+
+        text.classList.add('category');
+
+        btn.innerText = 'delete';
+
+        categoriesList.appendChild(text);
+        text.appendChild(btn);
+
+    })
+
+
+}
+
+const inputCategory: HTMLInputElement = document.createElement('input');
+const labelCategory: HTMLLabelElement = document.createElement('label');
+
+
+categoriesList.addEventListener('click', (e) => {
+    const htmlElement: HTMLElement = e.target as HTMLElement; 
+
+    if (htmlElement.tagName === 'BUTTON') {
+        const category = htmlElement.closest('.category');
+        const categoryText: string = category.innerHTML;
+
+        categories.forEach((category, index) => {
+
+            if (category.name === categoryText) {
+                categories.splice(index, 1);
+            }
+
+            const inputCategory: HTMLInputElement = document.querySelector(`#radio-${category.name}`);
+            const labelCategory: HTMLLabelElement = document.querySelector(`#label-${category.name}`);
+
+            inputCategory.remove();
+            labelCategory.remove();
+
+        })
+
+        category.remove();
+    };
+});
+
+const renderCategoriesToApp = () => {
+    categories.forEach( category => {
+        const inputCategory: HTMLInputElement = document.createElement('input');
+        const labelCategory: HTMLLabelElement = document.createElement('label');
+
+        inputCategory.setAttribute('type', 'radio');
+        inputCategory.setAttribute('id', `radio-${category.name}`);
+        inputCategory.setAttribute('name', 'category');
+        inputCategory.setAttribute('title', category.color);
+        inputCategory.value = category.name;
+
+        labelCategory.setAttribute('for', `radio-${category.name}`);
+        labelCategory.setAttribute('id', `label-${category.name}`)
+        labelCategory.style.color = category.color;
+        labelCategory.innerHTML = category.name.toUpperCase();
+
+        sectionCategories.appendChild(inputCategory);
+        sectionCategories.appendChild(labelCategory);
+    })
+}
